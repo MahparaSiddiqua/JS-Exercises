b9f731fc0547cd89f7b1d042f88364ead4d7dfc2
M_Siddiqua <mahpara.arthi@gmail.com>
2018-05-21 04:56:07 +0000

    Task 4

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ function sumArray(arr){
 }
 console.log(sumArray([1,2,3,4,5]));
 console.log(sumArray([1,20,-3,45]));
+
 //Task 3
+
 function checkEmail(emailString){
     var emailFormat= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     var match = emailFormat.test(emailString);
@@ -20,3 +22,27 @@ function checkEmail(emailString){
 }
 console.log(checkEmail("john@smith.com"));
 console.log(checkEmail("johnsmith.com"));
+
+//Task 4
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+
+
+function getReadingStatus(index){
+    return library[index].readingStatus;
+}
+console.log(getReadingStatus(1));
